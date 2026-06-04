57a8cb1498c6540b89c014a57f46b6e145ea39c3
maksupi <vianvrick@gmail.com>
2026-06-04 17:53:03 +0700

    capstone 9 keranjang dll

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -18,11 +18,14 @@ $routes->group('produk', function ($routes) {   // ← hapus filter auth sementa
     $routes->post('/', 'ProdukController::create');
     $routes->post('edit/(:any)', 'ProdukController::edit/$1');
     $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+    $routes->get('download', 'ProdukController::download');
 });
 
 // === KERANJANG ROUTES ===
 $routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
-    $routes->get('/', 'TransaksiController::index');
-    $routes->post('/', 'TransaksiController::create');
-    $routes->get('delete/(:any)', 'TransaksiController::delete/$1');
+    $routes->get('', 'TransaksiController::index');
+    $routes->post('', 'TransaksiController::cart_add');
+    $routes->post('edit', 'TransaksiController::cart_edit');
+    $routes->get('delete/(:any)', 'TransaksiController::cart_delete/$1');
+    $routes->get('clear', 'TransaksiController::cart_clear');
 });
