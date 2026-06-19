32fff62b298f9513b97719af0820b612216b66cd
maksupi <vianvrick@gmail.com>
2026-06-19 19:48:15 +0700

    new webservis

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -37,6 +37,13 @@
       <?php
       }
       ?>
+
+      <li class="nav-item">
+        <a class="nav-link <?php echo (uri_string() == 'history') ? "" : "collapsed" ?>" href="history">
+            <i class="bi bi-person"></i>
+            <span>History</span>
+        </a>
+    </li><!-- End History Nav -->
       
     </ul>
 
